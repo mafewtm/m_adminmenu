d59516ee5042b10d4968ba94bc3cbe27116e0855
Matthew <22198949+MafewTM@users.noreply.github.com>
2024-12-21 16:54:53 -0500

    feat: env browser

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './main.css';
 
+import { isEnvBrowser } from './utils/misc';
+
+if (isEnvBrowser()) {
+  const root = document.getElementById('root');
+
+  // https://i.imgur.com/iPTAdYV.png - Night time img
+  root!.style.backgroundImage = 'url("https://i.imgur.com/3pzRj9n.png")';
+  root!.style.backgroundSize = 'cover';
+  root!.style.backgroundRepeat = 'no-repeat';
+  root!.style.backgroundPosition = 'center';
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
