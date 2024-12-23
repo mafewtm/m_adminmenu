167b80ece11b56f9f5df72a2f2885905ab3fd9d0
Matthew <22198949+MafewTM@users.noreply.github.com>
2024-12-22 21:39:12 -0500

    fix(web/routing): open index upon menu opening

--- web/src/components/adminmenu/AdminMenu.tsx
+++ web/src/components/adminmenu/AdminMenu.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Switch, Route } from 'wouter';
+import { useLocation, Switch, Route } from 'wouter';
 import { useNuiEvent } from '@/hooks/useNuiEvent';
 import { fetchNui } from '@/utils/fetchNui';
 import Navbar from './components/NavBar';
@@ -11,11 +11,15 @@ import Development from './pages/development/Development';
 
 export default function AdminMenu() {
   const [visible, setVisible] = useState(false);
+  const navigate = useLocation()[1];
 
-  useNuiEvent('setVisible', (status: boolean) => setVisible(status));
+  useNuiEvent('setVisible', (status: boolean) => {
+    navigate('/');
+    setVisible(status)
+  });
 
   const handleEscape = (e: KeyboardEvent) => {
-    if (visible &&e.key === 'Escape') {
+    if (visible && e.key === 'Escape') {
       setVisible(false);
       fetchNui('closeMenu');
     }
@@ -30,16 +34,16 @@ export default function AdminMenu() {
   }, [visible]);
 
   return (
-    <div className='flex-col h-10/12 w-7/12 rounded-md border-2 border-solid border-zinc-700 drop-shadow-md' style={{ display: visible ? 'flex' : 'none' }}>
+    <div className='flex-col h-4/6 w-3/6 rounded-md border-2 border-solid border-zinc-700 drop-shadow-md' style={{ display: visible ? 'flex' : 'none' }}>
       <div className='flex h-full w-full bg-zinc-800 gap-5 p-5'>
         <Navbar />
         <div className='flex h-full w-full'>
           <Switch>
-            <Route path='/' component={Home} />
-            <Route path='/players' component={Players} />
-            <Route path='/server' component={Server} />
-            <Route path='/vehicles' component={Vehicles} />
-            <Route path='/development' component={Development} />
+            <Route path='/'><Home /></Route>
+            <Route path='/players'><Players /></Route>
+            <Route path='/server'><Server /></Route>
+            <Route path='/vehicles'><Vehicles /></Route>
+            <Route path='/development'><Development /></Route>
           </Switch>
         </div>
       </div>
